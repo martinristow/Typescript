600a22532839a5ecd6e1b20844ffd67b776474ff
martinristow <martinristov084@gmail.com>
2025-04-08 19:34:14 +0200

    Implement task creation functionality from form input
    - Replaced API-based task loading with localStorage-based retrieval using getTasks()
    - Added form event listener for handling task creation on submit
    - Validated input fields and ensured due date is in the future
    - Constructed a task object based on form input and passed it to addTask()
    - Improved user input handling and interaction via form submission

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,7 +1,43 @@
-import {getAllTasks, showTasks} from "./models/tasks";
+import {TasksInterface} from "./interfaces/TasksInterface";
+import {addTask, getTasks} from "./storage/taskStorage";
 
 export {};
 
-const response = await getAllTasks();
+// const response = await getAllTasks();
+//
+// if(response){
+//     showTasks(response);
+// }
+getTasks();
 
-showTasks(response);
+const taskForm = document.getElementById("createTaskForm") as HTMLFormElement;
+taskForm.addEventListener("submit", (event: SubmitEvent): void => {
+
+    const taskTitle = document.getElementById("taskTitle") as HTMLInputElement;
+    const taskDescription = document.getElementById("taskDescription") as HTMLInputElement;
+    const taskDueDate = document.getElementById("taskDueDate") as HTMLInputElement;
+
+    if (!taskTitle.value.trim() || !taskDescription.value.trim() || !taskDueDate.value.trim()) {
+        alert("All input fields are required!");
+        return;
+    }
+
+    const now = new Date();
+    const dueComparedDate = new Date(taskDueDate.value);
+    console.log(dueComparedDate);
+    if (dueComparedDate < now) {
+        alert("Due date must be in future");
+        return;
+    }
+
+    const task: TasksInterface = {
+        title: taskTitle.value,
+        description: taskDescription.value,
+        due_date: taskDueDate.value,
+    };
+
+    addTask(task);
+
+
+    // console.log(taskTitle.value, taskDescription.value, taskDueDate.value);
+})
