aa759626a29ccc65858269df20823d57b4428a8f
martinristow <martinristov084@gmail.com>
2025-04-11 14:02:26 +0200

    Initial Commit

---
+++ React-with-TypeScript-Projects/Project1/src/App.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "./Components/Sidebar.tsx";
+import {Profile} from "./Components/Profile.tsx";
+
+
+const App = () => {
+    return (
+        <div>
+            <Sidebar />
+            <Profile/>
+        </div>
+    )
+}
+export default App
---
+++ project/src/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = "Martin";
+console.log(name);
