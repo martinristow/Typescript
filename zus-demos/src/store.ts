15a35ad15774a138aa3f6fb6a3c2d3abe3637a77
martinristow <martinristov084@gmail.com>
2025-04-11 19:15:52 +0200

    Create Zustand store with count state and actions
    - Initialized Zustand store to manage counter state with increment and decrement functions.
    - Defined CountState interface
    - Used create() from Zustand to build store
    - Implemented count, increment(), and decrement()

---
+++ zus-demos/src/store.ts
@@ -0,0 +1,15 @@
+import {create} from 'zustand'
+
+interface CountState {
+    count: number
+    increment: () => void
+    decrement: () => void
+}
+
+export const useCounter = create<CountState>((set) => ({
+    count: 0,
+
+    increment: () => set(state => ({count: state.count + 1})),
+    decrement: () => set(state => ({count: state.count - 1}))
+
+}))
