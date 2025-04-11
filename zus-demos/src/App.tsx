b83f4a7807e85ec300c8cf6836562a76afd6d1c4
martinristow <martinristov084@gmail.com>
2025-04-11 19:16:31 +0200

    Display counter value in App and render OtherComponent
    - Used Zustand store in the main App component to display count and included OtherComponent.
    - Accessed count state from useCounter
    - Displayed current count value in a heading
    - Rendered OtherComponent for updating the count

--- zus-demos/src/App.tsx
+++ zus-demos/src/App.tsx
@@ -1,6 +1,15 @@
+import {useCounter} from "./store.ts";
+import OtherComponent from "./Components/OtherComponent.tsx";
+
 const App = () => {
+
+    // const count = useCounter(state => state.count) // first way to access data
+    const {count} = useCounter();
     return (
-        <div>App</div>
+        <div>
+            <h1>Count: {count}</h1>
+            <OtherComponent/>
+        </div>
     )
 }
 export default App
