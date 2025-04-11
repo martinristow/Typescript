4be10062d01025f9f3e4e0451cfe6d5edf246f06
martinristow <martinristov084@gmail.com>
2025-04-11 19:55:22 +0200

    Exercise for Zustand

--- zus-demos/src/App.tsx
+++ zus-demos/src/App.tsx
@@ -1,14 +1,19 @@
-import {useCounter} from "./store.ts";
-import OtherComponent from "./Components/OtherComponent.tsx";
+// import {useCounter} from "./store.ts";
+// import OtherComponent from "./Components/OtherComponent.tsx";
+
+// import CounterComponent from "./Components/CounterComponent.tsx";
+import LanguageSwitcherStoreComponent from "./Components/LanguageSwitcherStoreComponent.tsx";
 
 const App = () => {
 
     // const count = useCounter(state => state.count) // first way to access data
-    const {count} = useCounter();
+    // const {count} = useCounter();
     return (
         <div>
-            <h1>Count: {count}</h1>
-            <OtherComponent/>
+            {/*<h1>Count: {count}</h1>*/}
+            {/*<OtherComponent/>*/}
+            {/*<CounterComponent/>*/}
+            <LanguageSwitcherStoreComponent/>
         </div>
     )
 }
