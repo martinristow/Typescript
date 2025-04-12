9ffc17bf180cde557f255d08c1cb021d2d6e5db7
martinristow <martinristov084@gmail.com>
2025-04-12 13:44:56 +0200

    Integrate RecipeApp component into main App
    - Imported and rendered RecipeApp as the main application view
    - Ensured clean and minimal structure for App component

--- zus-projects/RecipeApp/src/App.tsx
+++ zus-projects/RecipeApp/src/App.tsx
@@ -1,8 +1,10 @@
+import RecipeApp from "./components/RecipeApp.tsx";
+
 const App = () => {
     return (
-        <div><h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1></div>
+        <div>
+            <RecipeApp/>
+        </div>
     )
 }
 export default App
