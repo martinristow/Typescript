c7a20c4052bcace7a5f3419ba84eb5a0b547863c
martinristow <martinristov084@gmail.com>
2025-04-13 23:22:52 +0200

    add StaleTime component with staleTime option in useQuery
    - Created a new component that fetches data from JSONPlaceholder
    - Configured useQuery with a 5-second staleTime to demonstrate caching behavior

---
+++ TanStack-Query/src/components/StaleTime.tsx
@@ -0,0 +1,30 @@
+import {useQuery} from "@tanstack/react-query";
+
+const fetchData = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    if (!response.ok) throw new Error('Network response was not ok!');
+
+    return await response.json();
+
+}
+
+const StaleTime = () => {
+
+    const {data, error, isLoading} = useQuery({
+        queryKey: ['todo'],
+        queryFn: fetchData,
+        staleTime: 5000
+    })
+
+    if (isLoading) return <h1>Loading...</h1>
+
+    if (error) return <h1>An Error occurred: {error.message}</h1>;
+
+    return (
+        <div>
+            <h1>Data</h1>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+    )
+}
+export default StaleTime
