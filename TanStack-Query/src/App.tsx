0499d2aa6037d7da12cf7ee8b42e878bbbf4e184
martinristow <martinristov084@gmail.com>
2025-04-13 20:11:06 +0200

    include WithTanStackQuery component in App
    - Commented out WithoutTanStackQuery
    - Added WithTanStackQuery to main component tree

--- TanStack-Query/src/App.tsx
+++ TanStack-Query/src/App.tsx
@@ -1,9 +1,13 @@
-import WithoutTanStackQuery from "./components/WithoutTanStackQuery.tsx";
+// import WithoutTanStackQuery from "./components/WithoutTanStackQuery.tsx";
+
+
+import WithTanStackQuery from "./components/WithTanStackQuery.tsx";
 
 const App = () => {
     return (
         <div>
-            <WithoutTanStackQuery/>
+            {/*<WithoutTanStackQuery/>*/}
+            <WithTanStackQuery/>
         </div>
     )
 }
