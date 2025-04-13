095e9da8ce1bb44e258ad7360221f9c3e61a1530
martinristow <martinristov084@gmail.com>
2025-04-13 23:04:24 +0200

    integrate ReactQueryDevtools into application
    - Imported ReactQueryDevtools from @tanstack/react-query-devtools
    - Rendered devtools inside QueryClientProvider
    - Set initialIsOpen to false for better user experience

--- TanStack-Query/src/main.tsx
+++ TanStack-Query/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 // Create a client
 const queryClient = new QueryClient()
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <QueryClientProvider client={queryClient}>
           <App/>
+          <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
   </StrictMode>,
 )
