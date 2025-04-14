77812436d74e4ad0ff3155a679681886d49f8601
martinristow <martinristov084@gmail.com>
2025-04-14 23:40:03 +0200

    add automatic and manual todo re-fetching with interval
    - Implemented fetching of todos using React Query and `useQuery`
    - Set up a 5-second interval to automatically fetch the next todo
    - Added a button to manually fetch the next todo
    - Used JSONPlaceholder API for todo data
    - Handled loading and error states

--- TanStack-Query/src/components/ReFetchInterval.tsx
+++ TanStack-Query/src/components/ReFetchInterval.tsx
@@ -1,8 +1,49 @@
+import {useEffect, useState} from "react";
+import {useQuery} from "@tanstack/react-query";
+
+
+const fetchTodo = async (id: number) => {
+
+    const response = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${id}`
+    );
+
+    if (!response.ok) throw new Error(`Network reponse was not ok. ${response.statusText}`);
+
+    return response.json();
+}
 
 
 const ReFetchInterval = () => {
+
+    const [currentId, setCurrentId] = useState<number>(1);
+
+    const {data, error, isLoading} = useQuery({
+        queryKey: ['todo', currentId],
+        queryFn: () => fetchTodo(currentId),
+        refetchInterval: 5000,
+    })
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentId(prevId => prevId < 200 ? prevId + 1 : 1);
+        }, 5000)
+
+        return () => clearInterval(interval);
+    }, []);
+
+
+    if (isLoading) return <h1>Loading...</h1>
+    if (error) return <h1>An Error occurred: {error.message}</h1>;
+
+    console.log(data)
     return (
-        <div>ReFetchInterval</div>
+        <div>
+            <h1>Todo</h1>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+            <button onClick={() => setCurrentId(prevId => prevId < 200 ? prevId + 1 : 1)}>Next Todo</button>
+        </div>
     )
 }
 export default ReFetchInterval
