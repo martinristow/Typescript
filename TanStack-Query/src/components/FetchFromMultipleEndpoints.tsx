649c10220197ddebaabc0f7c4d5a2424b882c9b7
martinristow <martinristov084@gmail.com>
2025-04-15 23:51:32 +0200

    Add FetchFromMultipleEndpoints component using React Query
    - Created a new component `FetchFromMultipleEndpoints.tsx`
    - Used `useQueries` from React Query to fetch data from two endpoints: /todos and /posts
    - Displayed a specific todo by `currentTodoId` and all posts data
    - Handled loading and error states

---
+++ TanStack-Query/src/components/FetchFromMultipleEndpoints.tsx
@@ -0,0 +1,53 @@
+import {useState} from "react";
+import {useQueries} from "@tanstack/react-query";
+
+const fetchTodos = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    if (!response.ok) throw new Error(response.statusText);
+    return response.json()
+}
+
+const fetchPosts = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    if (!response.ok) throw new Error(response.statusText);
+    return response.json()
+}
+
+
+const FetchFromMultipleEndpoints = () => {
+
+    const [currentTodoId, setCurrentTodoId] = useState(1)
+    const [currentPostId, setCurrentPostId] = useState(1)
+
+
+    const results = useQueries({
+        queries: [
+            {
+                queryKey: ['todos'],
+                queryFn: fetchTodos
+            },
+            {
+                queryKey: ['posts'],
+                queryFn: fetchPosts
+            }
+        ]
+    })
+
+    const [todosQuery, postsQuery] = results
+
+    if(todosQuery.isLoading || postsQuery.isLoading) return <h1>Loading...</h1>
+    if(todosQuery.error || postsQuery.error) return <h1>An error occurred: {todosQuery.error?.message || postsQuery.error?.message}</h1>
+
+
+    const todosData = todosQuery.data;
+    const postsData = postsQuery.data;
+
+    return (
+        <div>
+            <h1>Todos</h1>
+            <pre>{JSON.stringify(todosData.find((todo: any) => todo.id === currentTodoId ), null, 2)}</pre>
+            <pre>{JSON.stringify(postsData)}</pre>
+        </div>
+    )
+}
+export default FetchFromMultipleEndpoints
